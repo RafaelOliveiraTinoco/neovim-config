f75516fa580c8550bc02b966f633b64440c3e85e
Rafael Oliveira Tinoco <116078266+RafaelOliveiraTinoco@users.noreply.github.com>
2023-09-04 14:50:13 +0100

    Delete Config

--- lua/RafaelOliveiraTinoco/autoCmd/autoCmd.lua
+++
@@ -1,6 +0,0 @@
-vim.cmd("" .. "set number") -- Display current line number on the left
-vim.cmd("" .. "set relativenumber") -- Relative number on the left
-
--- Colorscheme
-colorschemes = {"nightfox", "dayfox", "dawnfox", "duskfox", "nordfox", "terafox", "carbonfox"} -- Colorschemes list from the nightfox plugin
-vim.cmd("colorscheme " .. colorschemes[1]) -- Change colorscheme
--- lua/RafaelOliveiraTinoco/config.lua
+++
@@ -1,2 +0,0 @@
-require("RafaelOliveiraTinoco.plugins.plugins") -- init and config plugins
-require("RafaelOliveiraTinoco.autoCmd.autoCmd") -- commands automatically executed on neovim startup
