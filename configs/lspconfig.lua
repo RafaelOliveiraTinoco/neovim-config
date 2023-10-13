9e152b5e3a0af30f69fc24e301016e11fa9ac3dc
Rafael Oliveira Tinoco <116078266+RafaelOliveiraTinoco@users.noreply.github.com>
2023-10-13 15:51:30 +0100

    Update lspconfig.lua

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -5,14 +5,6 @@ local capabilities = configs.capabilities
 local lspconfig = require "lspconfig"
 local servers = {
 
-    -- Download more LSP:
-        -- 1. Use :Mason command press Ctrl + F and find the ID of the language you want
-        -- 2. Go to the bottom of the page with upper "G" type the ID of the language and type enter
-        -- 3. Press "2" to switch to the LSP tab, place your coursor above the LSP you want to download and press the "i" keys
-        -- 4. To make the LSP usable place your coursor above the installed LSP and press enter, you will find the executable name
-        -- 5. Open this url: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md press Ctrl + F and search for that executable name
-        -- 6. Copy the text after "require'lspconfig'." and before ".setup{}" Example: require'lspconfig'.tsserver.setup{}
-
     -- Frontend development
         "html", -- html-lsp in Mason
         "tsserver", -- typescript-language-server in Mason
